2a65c51052282af88ae4736d319fa37350f539fc
YouKnowWhoIs <leokatmax@gmail.com>
2024-10-30 18:27:34 +0200

    refactoring api

--- src/app/(admin)/dashboard/@countries/page.tsx
+++ src/app/(admin)/dashboard/@countries/page.tsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import clsx from 'clsx';
 import Image from 'next/image';
-import { getSummaryCountries } from '@/lib/api';
+import { getCountries, getCompanies } from '@/lib/api';
 import DashboardCard from '@/app/components/dashboard-card';
+import getCountById from '@/lib/utils/getCountById';
 
 export interface PageProps {}
 
 export default async function Page({}: PageProps) {
-  const data = await getSummaryCountries();
+  const countries = await getCountries();
+  const companies = await getCompanies();
+
+  const counts = getCountById(companies, 'countryId');
 
   return (
     <DashboardCard label="Categories of companies">
       <div className="flex items-end gap-2 pb-5 px-5">
         <div>
-          {data.map(({ countryId, countryTitle, count }) => (
+          {countries.map(({ id, title }) => (
             <p
-              key={countryId}
+              key={id}
               className={clsx(
                 'text-sm text-gray-900 font-medium',
                 'before:inline-block before:w-2 before:h-2 before:rounded-full before:align-middle before:mr-2 before:bg-purple-200',
               )}
             >
-              {`${countryTitle} - ${count}`}
+              {`${title} - ${counts[id] || 0}`}
             </p>
           ))}
         </div>
